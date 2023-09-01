kimata1007/mozc-wai

commit 81d49eb9486ebebcb82cff978605fd16ff7aea6f

Migrate ScopedHandle to wil::unique_handle, cleanup
PiperOrigin-RevId: 556867946

src/base/run_level.cc
@@ -30,7 +30,6 @@
 #include "base/run_level.h"
 
 #include "base/logging.h"
-#include "absl/strings/string_view.h"
 
 #ifdef __linux__
 #include <sys/types.h>
@@ -38,14 +37,15 @@
 
 #ifdef _WIN32
 #include <aclapi.h>
+#include <wil/resource.h>
 #include <windows.h>
 
 #include "base/const.h"
 #include "base/system_util.h"
-#include "base/win32/scoped_handle.h"
 #include "base/win32/wide_char.h"
 #include "base/win32/win_sandbox.h"
 #include "base/win32/win_util.h"
+#include "absl/strings/string_view.h"
 #else  // _WIN32
 #include <unistd.h>
 #endif  // _WIN32
@@ -80,7 +80,7 @@ bool IsDifferentUser(const HANDLE hToken) {
 
   absl::string_view source_name(src.SourceName, TOKEN_SOURCE_LENGTH);
 
-  return source_name == kSeclogo|| source_name == kCredPro;
+  return source_name == kSeclogo || source_name == kCredPro;
 }
 
 // Returns true if UAC gave the high integrity level to the token
@@ -142,13 +142,12 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
   }
 
   // Get process token
-  HANDLE hProcessToken = nullptr;
+  wil::unique_process_handle process_token;
   if (!::OpenProcessToken(::GetCurrentProcess(),
-                          TOKEN_QUERY | TOKEN_QUERY_SOURCE, &hProcessToken)) {
+                          TOKEN_QUERY | TOKEN_QUERY_SOURCE,
+                          process_token.put())) {
     return RunLevel::DENY;
   }
-
-  ScopedHandle process_token(hProcessToken);
 
   // Opt out of elevated process.
   if (CLIENT == type && GetElevatedProcessDisabled() &&
@@ -157,17 +156,15 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
   }
 
   // Get thread token (if any)
-  HANDLE hThreadToken = nullptr;
+  wil::unique_process_handle thread_token;
   if (!::OpenThreadToken(::GetCurrentThread(), TOKEN_QUERY, TRUE,
-                         &hThreadToken) &&
+                         thread_token.put()) &&
       ERROR_NO_TOKEN != ::GetLastError()) {
     return RunLevel::DENY;
   }
 
-  ScopedHandle thread_token(hThreadToken);
-
   // Thread token (if any) must not a service account.
-  if (nullptr != thread_token.get()) {
+  if (!thread_token) {
     bool is_service_thread = false;
     if (!WinUtil::IsServiceUser(thread_token.get(), &is_service_thread)) {
       // Returns DENY conservatively.
@@ -188,7 +185,7 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
     }
 
     // Thread token must be created by sandbox.
-    if (nullptr == thread_token.get()) {
+    if (!thread_token) {
       return RunLevel::DENY;
     }
 
@@ -203,10 +200,10 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
     // See http://b/2301066 for details.
     const std::string user_dir = SystemUtil::GetUserProfileDirectory();
 
-    ScopedHandle dir_handle(::CreateFile(
+    wil::unique_hfile dir_handle(::CreateFile(
         win32::Utf8ToWide(user_dir).c_str(), READ_CONTROL | WRITE_DAC, 0,
         nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0));
-    if (nullptr != dir_handle.get()) {
+    if (!dir_handle) {
       BYTE buffer[sizeof(TOKEN_USER) + SECURITY_MAX_SID_SIZE];
       DWORD size = 0;
       if (::GetTokenInformation(thread_token.get(), TokenUser, buffer,
@@ -310,13 +307,13 @@ bool RunLevel::IsProcessInJob() {
 bool RunLevel::IsElevatedByUAC() {
 #ifdef _WIN32
   // Get process token
-  HANDLE hProcessToken = nullptr;
+  wil::unique_process_handle process_token;
   if (!::OpenProcessToken(::GetCurrentProcess(),
-                          TOKEN_QUERY | TOKEN_QUERY_SOURCE, &hProcessToken)) {
+                          TOKEN_QUERY | TOKEN_QUERY_SOURCE,
+                          process_token.put())) {
     return false;
   }
 
-  ScopedHandle process_token(hProcessToken);
   return mozc::IsElevatedByUAC(process_token.get());
 #else   // _WIN32
   return false;

src/base/BUILD.bazel
@@ -474,8 +474,8 @@ mozc_cc_library(
         "@com_google_absl//absl/types:span",
     ] + mozc_select(
         windows = [
-            "//base/win32:scoped_handle",
             "//base/win32:wide_char",
+            "//third_party/wil",
         ],
     ),
 )
@@ -532,7 +532,6 @@ mozc_cc_library(
     ] + mozc_select(
         windows = [
             ":util",
-            "//base/win32:scoped_handle",
             "//base/win32:wide_char",
         ],
     ),
@@ -621,10 +620,10 @@ mozc_cc_library(
         ios = ["//base/mac:mac_util"],
         macos = ["//base/mac:mac_util"],
         windows = [
-            "//base/win32:scoped_handle",
             "//base/win32:wide_char",
             # win_util.h includes gunit_prod.h for FRIEND_TEST.
             "//testing:gunit_prod",
+            "//third_party/wil",
         ],
     ),
 )
@@ -664,7 +663,7 @@ mozc_cc_library(
         macos = ["//base/mac:mac_process"],
         windows = [
             "//base/win32:wide_char",
-            "//base/win32:scoped_handle",
+            "//third_party/wil",
         ],
     ),
 )
@@ -725,9 +724,9 @@ mozc_cc_library(
         "@com_google_absl//absl/strings",
     ] + mozc_select(
         windows = [
-            "//base/win32:scoped_handle",
             "//base/win32:wide_char",
             "//base/win32:win_sandbox",
+            "//third_party/wil",
         ],
     ),
 )

src/base/file_util.cc
@@ -58,9 +58,9 @@
 #include <windows.h>  // Include windows.h before ktmw32.h
 #include <ktmw32.h>
 // clang-format on
+#include <wil/resource.h>
 
 #include "base/util.h"
-#include "base/win32/scoped_handle.h"
 #include "base/win32/wide_char.h"
 #else  // _WIN32
 #include <sys/stat.h>
@@ -355,9 +355,9 @@ namespace {
 absl::Status TransactionalMoveFile(const std::wstring &from,
                                    const std::wstring &to) {
   constexpr DWORD kTimeout = 5000;  // 5 sec.
-  ScopedHandle handle(
+  wil::unique_hfile handle(
       ::CreateTransaction(nullptr, 0, 0, 0, 0, kTimeout, nullptr));
-  if (handle.get() == 0) {
+  if (!handle) {
     const DWORD create_transaction_error = ::GetLastError();
     return absl::UnknownError(absl::StrFormat("CreateTransaction failed: %d",
                                               create_transaction_error));

src/base/mmap.cc
@@ -37,6 +37,7 @@
 #include "base/logging.h"
 #include "base/strings/zstring_view.h"
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
 #include "absl/types/span.h"
 
@@ -45,11 +46,11 @@
 #endif                           // __APPLE__
 
 #ifdef _WIN32
+#include <wil/resource.h>
 #include <windows.h>
 
 #include <limits>
 
-#include "base/win32/scoped_handle.h"
 #include "base/win32/wide_char.h"
 #else  // _WIN32
 #include <fcntl.h>
@@ -86,7 +87,7 @@ struct SyscallParams {
 };
 
 using FileDescriptor = HANDLE;
-using FdCloser = ScopedHandle;
+using FdCloser = wil::unique_hfile;
 
 absl::StatusOr<SyscallParams> GetSyscallParams(Mmap::Mode mode) {
   SyscallParams params;
@@ -157,16 +158,15 @@ constexpr std::pair<DWORD, DWORD> GetHiAndLo(size_t value) {
 absl::StatusOr<void *> MapFile(FileDescriptor fd, size_t offset, size_t size,
                                const SyscallParams &params) {
   const auto [max_size_hi, max_size_lo] = GetHiAndLo(size);
-  const HANDLE handle = ::CreateFileMapping(fd, 0, params.protect, max_size_hi,
-                                            max_size_lo, nullptr);
-  if (handle == nullptr) {
+  wil::unique_handle handle(::CreateFileMapping(
+      fd, 0, params.protect, max_size_hi, max_size_lo, nullptr));
+  if (!handle) {
     return absl::UnknownError(
         absl::StrFormat("Error %d: CreateFileMapping failed", GetLastError()));
   }
-  const ScopedHandle closer(handle);
 
   const auto [offset_hi, offset_lo] = GetHiAndLo(offset);
-  const LPVOID ptr = ::MapViewOfFile(handle, params.map_desired_access,
+  const LPVOID ptr = ::MapViewOfFile(handle.get(), params.map_desired_access,
                                      offset_hi, offset_lo, size);
   if (ptr == nullptr) {
     return absl::UnknownError(

src/base/process.cc
@@ -46,9 +46,9 @@
 #include "absl/time/time.h"
 
 #ifdef _WIN32
+#include <wil/resource.h>
 #include <windows.h>
 
-#include "base/win32/scoped_handle.h"
 #include "base/win32/wide_char.h"
 #include "base/win32/win_util.h"
 #else  // _WIN32
@@ -253,8 +253,9 @@ bool Process::WaitProcess(size_t pid, int timeout) {
 
 #ifdef _WIN32
   DWORD processe_id = static_cast<DWORD>(pid);
-  ScopedHandle handle(::OpenProcess(SYNCHRONIZE, FALSE, processe_id));
-  if (handle.get() == nullptr) {
+  wil::unique_process_handle handle(
+      ::OpenProcess(SYNCHRONIZE, FALSE, processe_id));
+  if (!handle) {
     LOG(ERROR) << "Cannot get handle id";
     return true;
   }
@@ -302,9 +303,9 @@ bool Process::IsProcessAlive(size_t pid, bool default_result) {
 
 #ifdef _WIN32
   {
-    ScopedHandle handle(
+    wil::unique_process_handle handle(
         ::OpenProcess(SYNCHRONIZE, FALSE, static_cast<DWORD>(pid)));
-    if (nullptr == handle.get()) {
+    if (!handle) {
       const DWORD error = ::GetLastError();
       if (error == ERROR_ACCESS_DENIED) {
         LOG(ERROR) << "OpenProcess failed: " << error;
@@ -343,9 +344,9 @@ bool Process::IsThreadAlive(size_t thread_id, bool default_result) {
   }
 
   {
-    ScopedHandle handle(
+    wil::unique_process_handle handle(
         ::OpenThread(SYNCHRONIZE, FALSE, static_cast<DWORD>(thread_id)));
-    if (nullptr == handle.get()) {
+    if (!handle) {
       const DWORD error = ::GetLastError();
       if (error == ERROR_ACCESS_DENIED) {
         LOG(ERROR) << "OpenThread failed: " << error;

src/base/win32/BUILD.bazel
@@ -143,9 +143,10 @@ mozc_cc_test(
     tags = MOZC_TAGS.WIN_ONLY,
     target_compatible_with = ["@platforms//os:windows"],
     deps = [
-        ":scoped_handle",
         ":win_sandbox",
         "//testing:gunit_main",
+        "@com_google_absl//absl/types:span",
+        "@com_microsoft_wil//:wil",
     ],
 )
 

src/base/win32/win_sandbox_test.cc
@@ -29,9 +29,15 @@
 
 #include "base/win32/win_sandbox.h"
 
-#include "base/win32/scoped_handle.h"
+#include <wil/resource.h>
+#include <windows.h>
+
+#include <cstddef>
+#include <vector>
+
 #include "testing/googletest.h"
 #include "testing/gunit.h"
+#include "absl/types/span.h"
 
 namespace mozc {
 namespace {
@@ -46,7 +52,7 @@ class TestableWinSandbox : public WinSandbox {
   using WinSandbox::GetSDDL;
 };
 
-void VerifySidContained(const std::vector<Sid> sids,
+void VerifySidContained(absl::Span<const Sid> sids,
                         WELL_KNOWN_SID_TYPE expected_well_known_sid) {
   Sid expected_sid(expected_well_known_sid);
   for (size_t i = 0; i < sids.size(); ++i) {
@@ -60,10 +66,9 @@ void VerifySidContained(const std::vector<Sid> sids,
 }
 
 TEST(WinSandboxTest, GetSidsToDisable) {
-  HANDLE process_token_ret = nullptr;
+  wil::unique_process_handle process_token;
   ::OpenProcessToken(::GetCurrentProcess(), TOKEN_ALL_ACCESS,
-                     &process_token_ret);
-  ScopedHandle process_token(process_token_ret);
+                     process_token.put());
 
   const std::vector<Sid> lockdown = WinSandbox::GetSidsToDisable(
       process_token.get(), WinSandbox::USER_LOCKDOWN);
@@ -93,10 +98,9 @@ TEST(WinSandboxTest, GetSidsToDisable) {
 }
 
 TEST(WinSandboxTest, GetPrivilegesToDisable) {
-  HANDLE process_token_ret = nullptr;
+  wil::unique_process_handle process_token;
   ::OpenProcessToken(::GetCurrentProcess(), TOKEN_ALL_ACCESS,
-                     &process_token_ret);
-  ScopedHandle process_token(process_token_ret);
+                     process_token.put());
 
   const std::vector<LUID> lockdown = WinSandbox::GetPrivilegesToDisable(
       process_token.get(), WinSandbox::USER_LOCKDOWN);
@@ -119,10 +123,9 @@ TEST(WinSandboxTest, GetPrivilegesToDisable) {
 }
 
 TEST(WinSandboxTest, GetSidsToRestrict) {
-  HANDLE process_token_ret = nullptr;
+  wil::unique_process_handle process_token;
   ::OpenProcessToken(::GetCurrentProcess(), TOKEN_ALL_ACCESS,
-                     &process_token_ret);
-  ScopedHandle process_token(process_token_ret);
+                     process_token.put());
 
   const std::vector<Sid> lockdown = WinSandbox::GetSidsToRestrict(
       process_token.get(), WinSandbox::USER_LOCKDOWN);

src/base/win32/win_util.cc
@@ -37,6 +37,7 @@
 #include <psapi.h>
 #include <shellapi.h>
 #include <stringapiset.h>
+#include <wil/resource.h>
 #include <winternl.h>
 
 #include <clocale>
@@ -47,7 +48,6 @@
 
 #include "base/logging.h"
 #include "base/system_util.h"
-#include "base/win32/scoped_handle.h"
 #include "base/win32/wide_char.h"
 #include "absl/base/call_once.h"
 #include "absl/status/status.h"
@@ -238,13 +238,12 @@ bool WinUtil::IsServiceProcess(bool *is_service) {
   }
 
   // Get process token
-  HANDLE hProcessToken = nullptr;
+  wil::unique_process_handle process_token;
   if (!::OpenProcessToken(::GetCurrentProcess(),
-                          TOKEN_QUERY | TOKEN_QUERY_SOURCE, &hProcessToken)) {
+                          TOKEN_QUERY | TOKEN_QUERY_SOURCE,
+                          process_token.put())) {
     return false;
   }
-
-  ScopedHandle process_token(hProcessToken);
 
   // Process token is one for a service account.
   if (!IsServiceUser(process_token.get(), is_service)) {
@@ -260,20 +259,18 @@ bool WinUtil::IsServiceThread(bool *is_service) {
   }
 
   // Get thread token (if any)
-  HANDLE hThreadToken = nullptr;
+  wil::unique_process_handle thread_token;
   if (!::OpenThreadToken(::GetCurrentThread(), TOKEN_QUERY, TRUE,
-                         &hThreadToken) &&
-      ERROR_NO_TOKEN != ::GetLastError()) {
+                         thread_token.put()) &&
+      ::GetLastError() != ERROR_NO_TOKEN) {
     return false;
   }
 
-  if (hThreadToken == nullptr) {
+  if (!thread_token) {
     // No thread token.
     *is_service = false;
     return true;
   }
-
-  ScopedHandle thread_token(hThreadToken);
 
   // Check if the thread token (if any) is one for a service account.
   if (!IsServiceUser(thread_token.get(), is_service)) {
@@ -329,12 +326,11 @@ bool WinUtil::IsProcessRestricted(HANDLE process_handle, bool *is_restricted) {
   }
   *is_restricted = false;
 
-  HANDLE token = nullptr;
-  if (!::OpenProcessToken(process_handle, TOKEN_QUERY, &token)) {
+  wil::unique_process_handle process_token;
+  if (!::OpenProcessToken(process_handle, TOKEN_QUERY, process_token.put())) {
     return false;
   }
 
-  ScopedHandle process_token(token);
   ::SetLastError(NOERROR);
   if (::IsTokenRestricted(process_token.get()) == FALSE) {
     const DWORD error = ::GetLastError();
@@ -354,23 +350,15 @@ bool WinUtil::IsProcessInAppContainer(HANDLE process_handle,
   }
   *in_appcontainer = false;
 
-  HANDLE token = nullptr;
+  wil::unique_process_handle process_token;
   if (!::OpenProcessToken(process_handle, TOKEN_QUERY | TOKEN_QUERY_SOURCE,
-                          &token)) {
+                          process_token.put())) {
     return false;
   }
 
-  // TokenIsAppContainer is defined only in Windows SDK 8.0 and later.
-  ScopedHandle process_token(token);
-  const TOKEN_INFORMATION_CLASS kTokenIsAppContainer =
-      static_cast<TOKEN_INFORMATION_CLASS>(29);  // TokenIsAppContainer
-#if defined(_WIN32_WINNT_WIN8)
-  static_assert(kTokenIsAppContainer == TokenIsAppContainer,
-                "Checking |kTokenIsAppContainer| has correct value.");
-#endif  // _WIN32_WINNT_WIN8
   DWORD returned_size = 0;
   DWORD retval = 0;
-  if (!GetTokenInformation(process_token.get(), kTokenIsAppContainer, &retval,
+  if (!GetTokenInformation(process_token.get(), TokenIsAppContainer, &retval,
                            sizeof(retval), &returned_size)) {
     return false;
   }
@@ -385,14 +373,12 @@ bool WinUtil::IsProcessInAppContainer(HANDLE process_handle,
 bool WinUtil::GetFileSystemInfoFromPath(zwstring_view path,
                                         BY_HANDLE_FILE_INFORMATION *info) {
   // no read access is required.
-  ScopedHandle handle(::CreateFileW(
+  wil::unique_hfile handle(::CreateFileW(
       path.c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
       nullptr, OPEN_EXISTING,
       FILE_FLAG_BACKUP_SEMANTICS | FILE_ATTRIBUTE_NORMAL, nullptr));
 
-  // Caveats: handle.get() returns nullptr when it is initialized with
-  //     INVALID_HANDLE_VALUE.
-  if (handle.get() == nullptr) {
+  if (!handle) {
     return false;
   }
   return !!::GetFileInformationByHandle(handle.get(), info);
@@ -419,14 +405,12 @@ bool WinUtil::GetNtPath(zwstring_view dos_path, std::wstring *nt_path) {
 
   nt_path->clear();
 
-  ScopedHandle file_handle(::CreateFileW(
+  wil::unique_hfile file_handle(::CreateFileW(
       dos_path.c_str(), 0,
       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr,
       OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_ATTRIBUTE_NORMAL,
       nullptr));
-  if (file_handle.get() == nullptr) {
-    // Caveats: |file_handle.get()| becomes nullptr instead of
-    // INVALID_HANDLE_VALUE when failure.
+  if (!file_handle) {
     return false;
   }
 
@@ -451,10 +435,10 @@ bool WinUtil::GetProcessInitialNtPath(DWORD pid, std::wstring *nt_path) {
   }
   nt_path->clear();
 
-  ScopedHandle process_handle(
+  wil::unique_process_handle process_handle(
       ::OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid));
 
-  if (process_handle.get() == nullptr) {
+  if (!process_handle) {
     VLOG(1) << "OpenProcess() failed: " << ::GetLastError();
     return false;
   }

commit d3596b580e0d9d367962f3f1f5f008a23a6c9c21

Fix 'RunLevel::GetRunLevel()' in Windows
This is a follow up to 81d49eb9486ebebcb82cff978605fd16ff7aea6f, which
accidentally broke 'RunLevel::GetRunLevel()' in Windows due to
unexpected condition flips.

Closes #798.

PiperOrigin-RevId: 561833990

src/base/run_level.cc
@@ -164,7 +164,7 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
   }
 
   // Thread token (if any) must not a service account.
-  if (!thread_token) {
+  if (thread_token) {
     bool is_service_thread = false;
     if (!WinUtil::IsServiceUser(thread_token.get(), &is_service_thread)) {
       // Returns DENY conservatively.
@@ -203,7 +203,7 @@ RunLevel::RunLevelType RunLevel::GetRunLevel(RunLevel::RequestType type) {
     wil::unique_hfile dir_handle(::CreateFile(
         win32::Utf8ToWide(user_dir).c_str(), READ_CONTROL | WRITE_DAC, 0,
         nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0));
-    if (!dir_handle) {
+    if (dir_handle) {
       BYTE buffer[sizeof(TOKEN_USER) + SECURITY_MAX_SID_SIZE];
       DWORD size = 0;
       if (::GetTokenInformation(thread_token.get(), TokenUser, buffer,